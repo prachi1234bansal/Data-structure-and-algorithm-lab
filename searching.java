973f4ca051c22df071e0294bf755da740306e2e5
prachi1234bansal <52649082+prachi1234bansal@users.noreply.github.com>
2020-01-09 23:21:46 +0530

    Add files via upload

---
+++ searching.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class searching
+{
+	public static void main(String args[])
+	{
+		int array[]=new int[10];
+		int i;
+		for(i=0;i<array.length;i++)
+		{
+			array[i]=Integer.parseInt(args[i]);
+		}
+		System.out.println("Enter the element which you want to search");
+		Scanner scx=new Scanner(System.in);
+		int searching_element=scx.nextInt();
+		for( i=0;i<array.length;i++)
+		{
+			if (searching_element==array[i])
+			{
+				 searching_element=1;
+				break;
+			}
+		}
+		if(searching_element==1)
+		{
+			System.out.println("Element is present");
+		}
+		else
+		{
+			System.out.println("Element is not present");
+		}
+}
+}
