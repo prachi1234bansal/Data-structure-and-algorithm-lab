2bea25a509c9dc6aa85c8b9a339cdd7b2198a423
prachi1234bansal <52649082+prachi1234bansal@users.noreply.github.com>
2020-01-09 23:44:40 +0530

    Add files via upload

---
+++ sorting.java
@@ -0,0 +1,25 @@
+class sorting
+{
+	public static void main(String args[])
+	{
+		int array[]=new int[]{1,7,9,3};
+		//elements are in ascending order;
+		int i,j,temp;
+		for(i=1;i<(array.length);i++)
+		{
+			for(j=0;j<(array.length)-1;j++)
+			{
+				if(array[j]>array[j+1])
+				{
+					temp=array[j];
+					array[j]=array[j+1];
+					array[j+1]=temp;
+				}
+			}
+		}
+		for(i=0;i<(array.length);i++)
+		{
+				System.out.print(array[i]+" ");
+		}
+	}
+}
