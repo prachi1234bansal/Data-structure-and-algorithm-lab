2a6658ca46a266cd9ce0edb959086be5c51da7fb
prachi1234bansal <52649082+prachi1234bansal@users.noreply.github.com>
2020-01-09 22:36:08 +0530

    Add files via upload

---
+++ deletion.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class deletion
+{
+	public static void main(String args[])
+	{
+	int i;
+	int array[]=new int[10];
+		for(i=0;i<array.length;i++)
+		{
+			array[i]=Integer.parseInt(args[i]);
+		}
+		System.out.println("Enter the position: ");
+		Scanner sx=new Scanner(System.in);
+		int position=sx.nextInt();
+		for(i=position;i<(array.length)-1;i++)
+		{
+			array[i]=array[i+1];
+		}
+		System.out.println("Deletion has been successfull: " );
+		for(i=0;i<(array.length)-1;i++)
+		{
+			System.out.print(array[i]+" ");
+		}
+	}
+}
